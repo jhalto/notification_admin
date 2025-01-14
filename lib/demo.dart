3b7cafa5adeb18eac334d8f606ecf46d292671f6
Zobayer Arman Nadim <zobayerarmannadim@gmail.com>
2025-01-14 17:17:05 +0600

    more

--- lib/demo.dart
+++ lib/demo.dart
@@ -7,7 +7,16 @@ class Demo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("Hello"),
+        child: Column(
+          children: [
+            Text(
+              "hello"
+            ),
+            Text(
+              "hello"
+            )
+          ],
+        ),
       ),
     );
   }
